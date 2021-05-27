7c499e7e9549f43bfe981b519ccd76d5652a2e9d
bluePlatinum <jukic.rok@gmail.com>
2021-05-27 17:08:11 +0200

    created clean task

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import os
 import platform
 from invoke import task
 
@@ -12,6 +13,44 @@ def build(c, target):
             c.run("make -C ./docs html")
         elif platform.system() == "Windows":
             c.run(".\\docs\\make.bat html")
+
+
+@task
+def clean(c, target):
+    if target == "all":
+        clean_build(c)
+        clean_dist(c)
+        clean_tox(c)
+    elif target == "build":
+        clean_build(c)
+    elif target == "dist":
+        clean_dist(c)
+    elif target == "tox":
+        clean_tox(c)
+
+
+@task
+def clean_build(c):
+    if platform.system() == "Linux":
+        c.run("rm -r ./build") if os.path.isdir("./build") else False
+    elif platform.system() == "Windows":
+        c.run("rd /s /q .\\build") if os.path.isdir(".\\build") else False
+
+
+@task
+def clean_dist(c):
+    if platform.system() == "Linux":
+        c.run("rm -r ./dist") if os.path.isdir("./dist") else False
+    elif platform.system() == "Windows":
+        c.run("rd /s /q .\\dist") if os.path.isdir(".\\dist") else False
+
+
+@task
+def clean_tox(c):
+    if platform.system() == "Linux":
+        c.run("rm -r ./.tox") if os.path.isdir("./.tox") else False
+    elif platform.system() == "Windows":
+        c.run("rd /s /q .\\.tox") if os.path.isdir(".\\.tox") else False
 
 
 @task
