d05ad886661a167decc399fa03e63ed5d8f57eaf
bluePlatinum <jukic.rok@gmail.com>
2021-05-27 16:25:34 +0200

    moved version, package name, author and author-mail definitions to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 import setuptools
-from pybacked import __version__
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pybacked",
-    version=__version__,
-    author="Roko Jukic",
-    author_email="jukic.rok@gmail.com",
     description="a simple backup utility",
     long_description=long_description,
     long_description_content_type="text/markdown",
