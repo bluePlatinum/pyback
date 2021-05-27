55e4c1083a309a5cb20ade92ca68fd6853b6ed5c
bluePlatinum <jukic.rok@gmail.com>
2021-05-28 00:57:52 +0200

    changed variable naming to improve readability

--- src/pybacked/storagehandling/zip_handler.py
+++ src/pybacked/storagehandling/zip_handler.py
@@ -2,22 +2,24 @@ import os
 import zipfile
 
 
-def zip_write(filepath, filedict, compression, compressionlevel):
+def zip_write(archivepath, filedict, compression, compressionlevel):
     """Write filedict to zip-archive. Will check wether file at filepath exists
     before writing. If file exists will return False, else will return True
     after complete write.
 
     :param filepath: the path to the file
-    :param filedict: dictionary containing the file, filename key-value pairs
+    :param filedict: dictionary containing the filepath, filename key-value
+    pairs
     :param compression: desired compression methods (see zipfile documentation)
     :param compressionlevel: compression level (see zipfile documentation)
     :return:
     """
-    if os.path.isfile(filepath):
+    if os.path.isfile(archivepath):
         return False
     else:
-        archive = zipfile.ZipFile(filepath, mode='x', compression=compression,
+        archive = zipfile.ZipFile(archivepath, mode='x',
+                                  compression=compression,
                                   compresslevel=compressionlevel)
-        for file, filename in filedict:
-            archive.write(file, arcname=filename)
+        for filepath, filename in filedict:
+            archive.write(filepath, arcname=filename)
     return True
