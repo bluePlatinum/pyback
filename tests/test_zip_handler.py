f82d3ecd6bfbd2e65d319cb07a312236263f8199
bluePlatinum <jukic.rok@gmail.com>
2021-05-29 15:57:42 +0200

    created test for zip_handler.read_bin() // Solves #21

--- tests/test_zip_handler.py
+++ tests/test_zip_handler.py
@@ -23,3 +23,24 @@ def test_zip_write():
         control_file.close()
 
     assert probe_data == control_data
+
+
+def test_read_bin():
+    datadir = "./tests/testdata/"
+    filelist = ["test_sample1.txt", "test_sample2.txt"]
+    archivepath = osp.abspath(datadir + "test_archive2_deflate.zip")
+    datadict = zip_handler.read_bin(archivepath, filelist)
+
+    control_file1 = io.open(osp.abspath(datadir + filelist[0]), "rb")
+    control_data1 = control_file1.read()
+    control_file1.close()
+
+    control_file2 = io.open(osp.abspath(datadir + filelist[1]), "rb")
+    control_data2 = control_file2.read()
+    control_file2.close()
+
+    assertions = [False, False]
+    assertions[0] = (datadict[filelist[0]] == control_data1)
+    assertions[1] = (datadict[filelist[1]] == control_data2)
+
+    assert assertions == [True, True]
