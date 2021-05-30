d7a9b5e09200f9c2a368eadaf5f417319612ad46
bluePlatinum <jukic.rok@gmail.com>
2021-05-30 16:13:22 +0200

    inserted module path to sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 
 # -- Imports -----------------------------------------------------------------
 
-from pybacked import __version__
+from pybacked import __version__   # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
