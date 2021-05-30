f0fbcb77e99addbab463b622b2e356ca5d810b11
bluePlatinum <jukic.rok@gmail.com>
2021-05-30 19:19:58 +0200

    fixed small typo in description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    description="a simple backup utility",
+    description="A simple backup utility",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bluePlatinum/pybacked",
