cc909b2c6393d436cbaf88cada73e9cbfd65e5b0
bluePlatinum <jukic.rok@gmail.com>
2021-06-01 21:00:57 +0200

    added diff-log.csv to test_archive2_deflate.zip

--- tests/test_restore.py
+++ tests/test_restore.py
@@ -30,3 +30,19 @@ def test_find_diff_failure():
     diff_entry = restore.find_diff(log_file, filename)
     log_file.close()
     assert diff_entry is None
+
+
+def test_find_diff_archive_success():
+    arch_path = os.path.abspath("./tests/testdata/test_archive2_deflate.zip")
+    filename = "test_sample1.txt"
+    expected_dict = {'filename': 'test_sample1.txt', 'modtype': '+',
+                     'diff': '1622567933.365362'}
+    diff_entry = restore.find_diff_archive(arch_path, filename)
+    assert diff_entry == expected_dict
+
+
+def test_find_diff_archive_failure():
+    arch_path = os.path.abspath("./tests/testdata/test_archive2_deflate.zip")
+    filename = "non_existent_file"
+    diff_entry = restore.find_diff_archive(arch_path, filename)
+    assert diff_entry is None
