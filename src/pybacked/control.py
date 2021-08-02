9ee8c444dd63a0e9ae3ad5a6026c0084a7691bcc
jukic.rok@gmail.com <jukic.rok@gmail.com>
2021-08-02 22:31:58 +0200

    created control.py module and added create_filedict function

---
+++ src/pybacked/control.py
@@ -0,0 +1,30 @@
+import os.path
+
+
+def create_filedict(diffcache, subdir=""):
+    """
+    Create a dictionary of filepath, filename key-value pairs for each enty
+    (and subdirectory-entry) in the DiffCache. Here the filename refers to the
+    name that the file is given in the archive and is derived from the
+    basename of the path.
+
+    :param diffcache: The DiffCache from which the dictionary should be
+        created.
+    :type diffcache: DiffCache
+    :param subdir: A subdirectory prefix, which will be prepended to the
+        filenames. This is mainly used for recursion.
+    :type subdir: str
+    :return: Returns a dictionary of filepath, filename key-value pairs which,
+        can be passed to zip_handler.archive_write().
+    :rtype: dict
+    """
+    dictionary = {}
+    for element in diffcache:
+        if element[2]:
+            sub_path = subdir + os.path.basename(element[0]) + "/"
+            sub_dict = create_filedict(element[1], subdir=sub_path)
+            for path, filename in sub_dict.items():
+                dictionary[path] = filename
+        else:
+            dictionary[element[0]] = subdir + os.path.basename(element[0])
+    return dictionary
