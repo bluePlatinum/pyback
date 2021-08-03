fcf95f7e6f58b7f01536430448eab38165547f04
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2021-08-04 01:19:31 +0200

    None diff handling (#97)
    * renamed ext_test/source to ext_test/storage
    
    * implemented filtering for None diff cases in diff.collect
    
    * skipping test_collect_none on non windows machines

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,4 +1,5 @@
 import time
+import platform
 import pytest
 from os.path import abspath as abspath
 from os.path import getmtime as getmtime
@@ -220,6 +221,25 @@ class TestCollect:
 
         diff_cache = diff.collect(storage, archive, DIFF_DATE)
         assert diff_cache == expected_cache
+
+    def test_collect_none(self):
+        """
+        Test if diff.collect removes None entries (entries for files, where no
+        change has been detected)
+        """
+        if platform.system() != "Windows":
+            pytest.skip("Wrong OS")
+
+        storage = abspath("./tests/testdata/ext_test/storage")
+        archive = abspath("./tests/testdata/ext_test/archive")
+
+        diffcache = diff.collect(storage, archive, DIFF_HASH, HASH_SHA256)
+
+        # check if None entries (doc1.txt, doc3.txt) were successully removed
+        subdir = abspath("./tests/testdata/ext_test/storage/subdir")
+
+        assert len(diffcache.diffdict) == 1
+        assert len(diffcache.diffdict[subdir].diffdict) == 2
 
 
 def test_diffcache_iter():
