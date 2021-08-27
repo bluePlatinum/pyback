81ebbde41ca2734e3617272907ecb04c2594c59e
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2021-08-27 15:26:42 +0200

    conifg serialization (#123)
    * fixed test_read_bin()
    
    * added get_dict() method to Configuration class
    
    * create serialize_config
    
    * create serialize_config_list() function
    
    * create write_config() function
    
    * created example config-file
    
    * create read_config function
    
    * added __eq__ method for Configuration class
    
    * added deserialize_config function
    
    * changed deserialize_config to deserialize config lists

--- src/pybacked/config.py
+++ src/pybacked/config.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Configuration:
     """
     Class to hold all the data of a specific backup configuration. This can be
@@ -36,3 +39,140 @@ class Configuration:
         self.compression_algorithm = compression_algorithm
         self.compresslevel = compresslevel
         self.hash_algorithm = hash_algorithm
+
+    def __eq__(self, other):
+        if self.name != other.name:
+            return False
+        elif self.storage != other.storage:
+            return False
+        elif self.archive != other.archive:
+            return False
+        elif self.diff_algorithm != other.diff_algorithm:
+            return False
+        elif self.compression_algorithm != other.compression_algorithm:
+            return False
+        elif self.compresslevel != other.compresslevel:
+            return False
+        elif self.hash_algorithm != other.hash_algorithm:
+            return False
+        else:
+            return True
+
+    def get_dict(self):
+        """
+        Get a dictionary with all fields of the Configuration class
+        represented as fieldname-value pairs.
+
+        :return: Returns the dictionary of all fields of the Configuration
+            class
+        :rtype: dict
+        """
+        name = self.name
+        storage = self.storage
+        archive = self.archive
+        diff_algorithm = self.diff_algorithm
+        compression_alg = self.compression_algorithm
+        compresslevel = self.compresslevel
+        hash_algorithm = self.hash_algorithm
+
+        configuration_dir = {"name": name, "storage": storage,
+                             "archive": archive,
+                             "diff_algorithm": diff_algorithm,
+                             "compression_algorithm": compression_alg,
+                             "compresslevel": compresslevel,
+                             "hash_algorithm": hash_algorithm}
+
+        return configuration_dir
+
+
+def deserialize_config(data):
+    """
+    Create instances of Configuration class from a json-string containging
+    config data.
+
+    :param data: The json string containing the configuration data
+    :type data: str
+    :return: A list with Configuration class objects.
+    :rtype: List
+    """
+    config_dict = json.loads(data)
+    config_list = list()
+    for config_name, config_data in config_dict.items():
+        current_config = config_dict[config_name]
+        config = Configuration(current_config['name'],
+                               current_config['storage'],
+                               current_config['archive'],
+                               current_config['diff_algorithm'],
+                               current_config['compression_algorithm'],
+                               current_config['compresslevel'],
+                               current_config['hash_algorithm'])
+        config_list.append(config)
+    return config_list
+
+
+def read_config(filepath):
+    """
+    Read the config from a given config file.
+
+    :param filepath: The path to the config file.
+    :type filepath: str
+    :return: The json deserialized list.
+    :rtype: list
+    """
+    file = open(filepath, 'r')
+    file_content = file.read()
+    config_list = json.loads(file_content)
+    file.close()
+    return config_list
+
+
+def serialize_config(config):
+    """
+    Returns the json-serialized string from Configuration object. This string
+    can then be directly written to the .json file.
+
+    :param config: The Configuration object
+    :type config: Configuration
+    :return: The json-serialized string of the Configuration object
+    :rtype: str
+    """
+    # write the fileds to variables for improved readability
+
+    json_string = json.dumps(config.get_dict())
+    return json_string
+
+
+def serialize_config_list(config_list):
+    """
+    Serialize a list of configs. This is used so that the user has the option
+    to save multiple configs in a single file.
+
+    :param config_list: A list of Configuration class objects
+    :type config_list: list
+    :return: A serialized json string which can directly be written to a config
+        file.
+    :rtype: str
+    """
+    config_dict = dict()
+    for element in config_list:
+        config_dict[element.name] = element.get_dict()
+    json_string = json.dumps(config_dict)
+    return json_string
+
+
+def write_config(config_list, filepath):
+    """
+    Get the json string returned by serialize_config_list and write it to a
+    given file.
+
+    :param config_list: A list of Configuration objects
+    :type config_list: list
+    :param filepath: The path to the config file, which will hold the config
+        information.
+    :type filepath: str
+    :return: void
+    :rtype: None
+    """
+    json_string = serialize_config_list(config_list)
+    file = open(filepath, 'w')
+    file.write(json_string)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,120 @@
+import json
+import os
 import pybacked.config
+import tempfile
 
 
-def test_configuration_class():
+class TestConfigurationClass:
+    def test_constructor(self):
+        instance = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                 hash_algorithm=7)
+        assert instance.name == "1"
+        assert instance.storage == "2"
+        assert instance.archive == "3"
+        assert instance.diff_algorithm == 4
+        assert instance.compression_algorithm == 5
+        assert instance.compresslevel == 6
+        assert instance.hash_algorithm == 7
+
+    def test_get_dict(self):
+        instance = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                 hash_algorithm=7)
+
+        # create expected dictionary
+        expected = {"name": "1", "storage": "2", "archive": "3",
+                    "diff_algorithm": 4, "compression_algorithm": 5,
+                    "compresslevel": 6, "hash_algorithm": 7}
+
+        result = instance.get_dict()
+        assert result == expected
+
+    def test_equal(self):
+        instance1 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                  hash_algorithm=7)
+        instance2 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                  hash_algorithm=7)
+        assert instance1 == instance2
+
+    def test_non_equal(self):
+        instance1 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                  hash_algorithm=7)
+        instance2 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                                  hash_algorithm=8)
+        assert instance1 != instance2
+
+
+def test_serialize_config():
     instance = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
                                              hash_algorithm=7)
-    assert instance.name == "1"
-    assert instance.storage == "2"
-    assert instance.archive == "3"
-    assert instance.diff_algorithm == 4
-    assert instance.compression_algorithm == 5
-    assert instance.compresslevel == 6
-    assert instance.hash_algorithm == 7
+    expected = json.dumps(instance.get_dict())
+    result = pybacked.config.serialize_config(instance)
+    assert result == expected
+
+
+def test_serialize_config_list():
+    instance1 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                              hash_algorithm=7)
+    instance2 = pybacked.config.Configuration("8", "9", "10", 11, 12, 13,
+                                              hash_algorithm=14)
+    config_list = [instance1, instance2]
+
+    # create expected
+    config_dict = dict()
+    for element in config_list:
+        config_dict[element.name] = element.get_dict()
+    expected = json.dumps(config_dict)
+
+    result = pybacked.config.serialize_config_list(config_list)
+    assert result == expected
+
+
+def test_write_string():
+    instance1 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                              hash_algorithm=7)
+    instance2 = pybacked.config.Configuration("8", "9", "10", 11, 12, 13,
+                                              hash_algorithm=14)
+    config_list = [instance1, instance2]
+
+    # create expected file content
+    expected = pybacked.config.serialize_config_list(config_list)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        file_path = os.path.abspath(tmpdir + "/custom.json")
+
+        # perform file write
+        pybacked.config.write_config(config_list, file_path)
+
+        file = open(file_path, 'r')
+        file_content = file.read()
+        file.close()
+
+        assert file_content == expected
+
+
+def test_read_config():
+    configfile_path = os.path.abspath("./tests/testdata/config-example.json")
+
+    # create expected values
+    expected = {'1': {'name': '1', 'storage': '2', 'archive': '3',
+                      'diff_algorithm': 4, 'compression_algorithm': 5,
+                      'compresslevel': 6, 'hash_algorithm': 7},
+                '8': {'name': '8', 'storage': '9', 'archive': '10',
+                      'diff_algorithm': 11, 'compression_algorithm': 12,
+                      'compresslevel': 13, 'hash_algorithm': 14},
+                '15': {'name': '15', 'storage': '16', 'archive': '17',
+                       'diff_algorithm': 18, 'compression_algorithm': 19,
+                       'compresslevel': 20, 'hash_algorithm': 21}}
+
+    result = pybacked.config.read_config(configfile_path)
+    assert result == expected
+
+
+def test_deserialize_config():
+    instance1 = pybacked.config.Configuration("1", "2", "3", 4, 5, 6,
+                                              hash_algorithm=7)
+    instance2 = pybacked.config.Configuration("8", "9", "10", 11, 12, 13,
+                                              hash_algorithm=14)
+    config_list = [instance1, instance2]
+    data = pybacked.config.serialize_config_list(config_list)
+    deserialized = pybacked.config.deserialize_config(data)
+    assert deserialized == config_list
