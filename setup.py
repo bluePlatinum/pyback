03aff8d6e0a33b4a17d53a125841253efe336af7
jukic.rok@gmail.com <jukic.rok@gmail.com>
2021-09-01 17:07:12 +0200

    added entry point for __main__.py:main to setup.py

--- setup.py
+++ setup.py
@@ -21,4 +21,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
+    entry_points={
+        "console_scripts": ["pybacked=pybacked.__main__:main"]
+    },
 )
